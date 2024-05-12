c5ba8ba884ed25de11e98a8c1c9366b0ebb2cd33
AntltNoProb <gingerant@sina.com>
2024-05-13 04:26:21 +0800

    feat: change css and add console hint

--- main.js
+++ main.js
@@ -20,5 +20,21 @@ async function x() {
 //         }
 //     })
 // }
+console.info('%cHints:', '\
+padding:2px 1rem 0px;\
+border-radius: 10% / 50%;\
+margin-bottom:-12px;\
+font-size: 1.05rem;\
+font-weight: bold;\
+color:#333;\
+text-shadow: 0 0 10px #fff,0 0 10px #fff,\
+    0 0 10px #fff,0 0 10px #fff,0 0 10px #fff;\
+outline: solid 1px #2c628b, 1px dotted #000;\
+background: rgb(104,178,218);\
+background: linear-gradient(0deg, rgba(104,178,218,1) 0%, \
+    rgba(152,209,239,1) 49%, rgba(196,229,246,1) 49%, rgba(229,244,252,1) 100%); \
+box-shadow: inset 0 0 0 2px #86c6e8, \
+    inset 0 0 0 1px dotted #000;');
 console.info('Maximize the console, paste the secret code and then press enter');
 console.info('把控制台窗口拉到最大，粘贴神秘代码并按下enter键');
+console.info()
